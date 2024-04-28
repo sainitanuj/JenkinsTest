80fd9f631d80150cde89398eb54880d8d382bcaa
Tanuj Kumar Saini <tanuj.saini@hitechglobal>
2024-04-28 15:04:06 +0530

    initail commit

---
+++ src/main/java/com/JenkinsTesApp/JenkinsTesAppApplication.java
@@ -0,0 +1,13 @@
+package com.JenkinsTesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JenkinsTesApp/JenkinsTesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JenkinsTesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
