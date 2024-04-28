7b73bb95ff849049e5f128c05204cf7eb205b11f
tanuj.saini <tanuj.saini@hitechroboticsystemz.com>
2024-04-28 15:18:56 +0530

    test controller added

---
+++ src/main/java/com/JenkinsTesApp/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.JenkinsTesApp.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test1")
+    public String testApp(){
+        return "test 1";
+
+    }
+
+}
