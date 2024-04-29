509191159452c1a69fadc8b28bc29af1fe2cba93
tanuj.saini <tanuj.saini@hitechroboticsystemz.com>
2024-04-29 12:52:41 +0530

    config updated- jar to war

--- src/main/java/com/JenkinsTesApp/JenkinsTesAppApplication.java
+++ src/main/java/com/JenkinsTesApp/JenkinsTesAppApplication.java
@@ -2,9 +2,15 @@ package com.JenkinsTesApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsTesAppApplication {
+public class JenkinsTesAppApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JenkinsTesAppApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsTesAppApplication.class, args);
